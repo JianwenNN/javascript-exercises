119b694e644f3469c5afde65df43b8054cee5e56
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-16 13:06:31 -0500

    Added solution to the fourth exercie

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+//check if array contains argument and return its position
+for (let arg of args) {
+    let pos = arr.indexOf(arg);
+    if (pos !== -1) {
+        arr.splice(pos, 1);
+    }
+    else if (pos == -1) continue;
+}
+return arr;
 };
 
 // Do not edit below this line
