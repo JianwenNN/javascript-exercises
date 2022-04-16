1254834ba28163cfaec48054ca705aa9e46035f1
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-16 13:51:29 -0500

    Optimized solution of the sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
-    if(year%400 == 0 || (year%4 == 0 && year%100 != 0)) return true;
-    else if(year%100 == 0 || year%4 != 0) return false;
+    return year%4 == 0 && (year%400 == 0 || year%100 != 0);
+    
 };
 
 // Do not edit below this line
