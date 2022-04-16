ee872458722805a47cd13c13776f8c35bcc4bea2
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-16 11:15:16 -0500

    Added completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatString = '';
+    if(num > 0) {
+        for(let i = 1; i <= num; i++) {
+            repeatString += string;
+        }
+    }
+    else if(num < 0) {
+        repeatString = 'ERROR';
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
