cef9aee8902e7a194bf329f08b7439693b3c2793
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-28 14:29:00 -0500

    Added solution to the palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let lowered = str.toLowerCase();
+    let arr = lowered.split('');
+    let letterArr = [];
+    for (let char of arr) {
+        if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+            letterArr.push(char);
+        }
+    }
+    let reversedArr = letterArr.slice().reverse();
+    let origin = letterArr.join('');
+    let reversed = reversedArr.join('');
+    if (origin === reversed) return true;
+    else return false;
 };
 
 // Do not edit below this line
