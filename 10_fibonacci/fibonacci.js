ac9dc096cdeb3570ccd7b76459c39433cbc4af05
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-28 15:33:29 -0500

    Added solution for getTheTitles exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArr = [1, 1];
+    if (+num < 0) return "OOPS";
+    else if (+num <= 2) return 1;
+    else {
+        for (let i = 2; i < +num; i++) {
+            fibArr[i] = fibArr[i-1] + fibArr[i-2];
+        }
+        return fibArr[+num-1];
+    }
 };
 
 // Do not edit below this line
