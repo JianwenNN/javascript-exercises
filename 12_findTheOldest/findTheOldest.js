4907a975375c4af28fcf1b3d5aea29e34a62e51e
Jianwen Xu <stephy.jxu@gmail.com>
2022-04-28 19:51:34 -0500

    Added solution to the find the oldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    for (let person of arr) {
+        if (!person.yearOfDeath) {
+            person.age = (new Date()).getFullYear() - person.yearOfBirth;
+        }
+        else person.age = person.yearOfDeath - person.yearOfBirth;
 
+    }
+    arr.sort(function(a, b) {
+        return a.age - b.age;
+    })
+    return arr[arr.length - 1];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
